10c77b4e64f776b240943e1ccfb60d3e9b1f3291
Rejuwane <rezwanrian1@gmail.com>
2023-10-20 04:01:40 +0600

    footer section complate without errorpage,readme etc

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 
 const MainLayout = () => {
@@ -7,6 +8,7 @@ const MainLayout = () => {
         <div className="max-w-screen-lg mx-auto bg-[#FFF]">
             <Navbar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+            <footer className="footer p-10 bg-[#a7d0a2] text-base-content rounded-b-lg mt-4 ">
+  <nav>
+    <header className="footer-title">Services</header> 
+    <a className="link link-hover">Branding</a> 
+    <a className="link link-hover">Design</a> 
+    <a className="link link-hover">Marketing</a> 
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Company</header> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Blog</a> 
+    <a className="link link-hover">Home</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Legal</header> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer> 
+
+        </div>
+        </div>
+    );
+};
+
+export default Footer;
