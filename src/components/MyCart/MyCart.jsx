cb3889aee9c8def16ef91fe4c4a0fea22386dde1
Rejuwane <rezwanrian1@gmail.com>
2023-10-20 03:03:27 +0600

    add cart and delete button success

--- src/components/MyCart/MyCart.jsx
+++ src/components/MyCart/MyCart.jsx
@@ -6,7 +6,7 @@ const MyCart = () => {
     const myCartData = useLoaderData()
     console.log(myCartData);
     return (
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
             {
                 myCartData.map(cart => <ShowCart key={cart._id} cart={cart}/>)
             }
--- src/components/MyCart/ShowCart.jsx
+++ src/components/MyCart/ShowCart.jsx
@@ -1,22 +1,105 @@
 
 
-const ShowCart = ({cart}) => {
-    const {name,photo,price,brand} = cart;
+import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+import Swal from "sweetalert2";
+import { Rating } from '@smastrom/react-rating'
 
-    return (
+import '@smastrom/react-rating/style.css'
+
+const Cart = ({ cart }) => {
+  const {
+    name,
+    photo,
+    price,
+    brand,
+    _id,
+    type,  
+    rating,
+    
+    
+  } = cart;
+
+  const handleDelete = (_id) => {
+    fetch(
+      `http://localhost:5000/cart/${_id}`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          Swal.fire(
+            {
+            title: "Delete",
+            text: "deleted successfully",
+            icon: "question",
+            confirmButtonText: "cool"
+            })
+        }
+        // navigate("/");
+      });
+  };
+
+  return (
+    <div>
+      <div className="md:grid grid-cols-3 rounded-xl my-3 md:my-auto shadow-lg items-center ">
         <div>
-     <div className="card card-compact bg-base-100 shadow-xl mb-4">
-        <figure>
-          <img src={photo} className="w-full h-[500px]" alt="" />
-        </figure>
-        <div className="card-body items-center">
-          <h2 className="card-title text-4xl font-bold ">{name}</h2>
-          <p className="text-2xl font-semibold">Price: {price}</p>
-          <p className="text-2xl font-medium">Brand: {brand}</p>
+          <img
+            className="w-full h-[200px] rounded-tr-xl rounded-tl-xl md:rounded-tl-xl md:rounded-bl-xl bg-gray-400"
+            src={photo}
+            alt=""
+          />
+        </div>
+        <div className="col-span-2 p-4  ">
+          <div className="flex">
+            <div className="flex-1">
+              <p className="font-bold text-xl">{name}</p>
+              <p className="font-medium">Brand: {brand}</p>
+              <p className="font-medium">Type: {type}</p>
+              <div className="">
+                <p className="font-medium">
+                  Price <span className="text-red-500">${price}</span>
+                </p>
+                <p className=" font-medium flex items-center">
+                Rating: <span className="text-blue-500"> {rating}</span>
+                <span><Rating style={{ maxWidth: 100 }} readOnly halfFillMode='svg' value={rating < 4.5 ? Math.floor(rating) : rating} /></span>
+                  
+                </p>
+               </div>
+           </div>
+<div className="flex flex-col justify-around items-center">
+              <Link>
+                <div
+                  className="badge badge-outline bg-blue-500 h-8 border-none rounded-md w-20 text-2xl"
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content="Delete!"
+                  onClick={() => handleDelete(_id)}
+                >
+                  <MdDelete />
+                </div>
+              </Link>
+              <Tooltip id="my-tooltip" />
+              <Link to={`/brand/${_id}`}>
+                <div
+                  className="badge badge-outline bg-amber-500 h-8 border-none rounded-md w-20 text-2xl"
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content="View Details!"
+                >
+                  <FaEye />
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
-export default ShowCart;
+export default Cart;
